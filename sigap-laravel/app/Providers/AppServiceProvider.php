e5beeb7fbf4410e588fd30368236c7d30da273ae
Bryan Siagian <bryanganz292@gmail.com>
2026-08-27 18:02:13 +0700

    fix: http for ngrok

--- sigap-laravel/app/Providers/AppServiceProvider.php
+++ sigap-laravel/app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (str(config('app.url'))->startsWith('https://')) {
+            \URL::forceScheme('https');
+        }
     }
 }
